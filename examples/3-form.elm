63a35328a6412da991e2a4faf1527954a74fe812
Abby Henríquez Tejera <paradoja@gmail.com>
2016-08-19 23:49:09 +0200

    Ex 3

--- examples/3-form.elm
+++ examples/3-form.elm
@@ -1,8 +1,10 @@
 import Html exposing (..)
 import Html.App as Html
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
-
+import Html.Events exposing (onInput, onClick)
+import String
+import List
+import Regex
 
 main =
   Html.beginnerProgram
@@ -20,12 +22,14 @@ type alias Model =
   { name : String
   , password : String
   , passwordAgain : String
+  , age : Int
+  , showErrors : Bool
   }
 
 
 model : Model
 model =
-  Model "" "" ""
+  Model "" "" "" 0 False
 
 
 
@@ -36,42 +40,110 @@ type Msg
     = Name String
     | Password String
     | PasswordAgain String
+    | Age Int
+    | ShowValidations
 
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
     Name name ->
-      { model | name = name }
+        { model | name = name }
 
     Password password ->
-      { model | password = password }
+        { model | password = password }
 
     PasswordAgain password ->
-      { model | passwordAgain = password }
+        { model | passwordAgain = password }
 
+    Age age ->
+        { model | age = age }
 
+    ShowValidations ->
+        { model | showErrors = True }
 
 -- VIEW
 
 
+getAgeInt : Int -> String -> Int
+getAgeInt default ageString =
+    case String.toInt ageString of
+        Ok age ->
+            age
+
+        Err _ ->
+            default
+
 view : Model -> Html Msg
 view model =
   div []
-    [ input [ type' "text", placeholder "Name", onInput Name ] []
-    , input [ type' "password", placeholder "Password", onInput Password ] []
-    , input [ type' "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
+    [ div [] [ input [ type' "text", placeholder "Name", onInput Name ] []
+             , input [ type' "password", placeholder "Password", onInput Password ] []
+             , input [ type' "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
+             , input [ type' "number", placeholder "Age", onInput (Age << getAgeInt 0) ] []
+             ]
+    , input [ type' "submit", name "Submit", onClick ShowValidations ] []
     , viewValidation model
     ]
 
 
+type Validation
+    = Valid
+    | Invalid (List String)
+
+type alias Validator = Model -> Validation
+
+combineValidators : Validation -> Validation -> Validation
+combineValidators v1 v2 =
+    case v1 of
+        Valid -> v2
+        Invalid reasons1 ->
+            case v2 of
+                Valid -> v1
+                Invalid reasons2 -> Invalid (reasons1 ++ reasons2)
+
+checkPasswordAgain : Validator
+checkPasswordAgain model =
+    if model.password == model.passwordAgain then
+        Valid
+    else
+        Invalid ["Passwords do not match!"]
+
+checkPasswordLength : Int -> Validator
+checkPasswordLength length model =
+    if String.length model.password >= length then
+        Valid
+    else
+        Invalid ["Password should be bigger than " ++ toString length ++ " chars!"]
+
+checkPasswordDiversity : List String -> Validator
+checkPasswordDiversity checks model =
+    let checker re = Regex.contains (Regex.regex re) model.password
+    in if List.all checker checks  then
+           Valid
+       else
+           Invalid ["Password should be diverse!!!"]
+
+validators : List (Model -> Validation)
+validators =
+    [ checkPasswordAgain
+    , checkPasswordLength 8
+    , checkPasswordDiversity ["[a-z]", "[A-Z]", "[0-9]"]
+    ]
+
 viewValidation : Model -> Html msg
 viewValidation model =
   let
-    (color, message) =
-      if model.password == model.passwordAgain then
-        ("green", "OK")
-      else
-        ("red", "Passwords do not match!")
+      validations = List.map ((|>) model) validators
+      validP = List.foldl combineValidators Valid validations
+      (color, messages) =
+          case validP of
+              Valid -> ("green", ["OK"])
+              Invalid reasons ->
+                  ("red", reasons)
   in
-    div [ style [("color", color)] ] [ text message ]
+      if model.showErrors then
+          div [ style [("color", color)] ] <|
+              List.map (\m-> div [] [ text m ]) messages
+      else
+          text ""
