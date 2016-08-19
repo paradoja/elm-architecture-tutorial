b1eabb4b449d0d07d6a330198a3d6b0eea1574ef
Abby Henríquez Tejera <paradoja@gmail.com>
2016-08-19 23:49:00 +0200

    Ex. 1

--- examples/1-button.elm
+++ examples/1-button.elm
@@ -31,16 +31,20 @@ model =
 type Msg
   = Increment
   | Decrement
+  | Reset
 
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
     Increment ->
-      model + 1
+        model + 1
 
     Decrement ->
-      model - 1
+        model - 1
+
+    Reset ->
+        0
 
 
 
@@ -50,7 +54,8 @@ update msg model =
 view : Model -> Html Msg
 view model =
   div []
-    [ button [ onClick Decrement ] [ text "-" ]
+    [ button [ onClick Reset ] [ text "Reset" ]
+    , button [ onClick Decrement ] [ text "-" ]
     , div [] [ text (toString model) ]
     , button [ onClick Increment ] [ text "+" ]
     ]
