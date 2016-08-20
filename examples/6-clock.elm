8a9af91761d86673441f3e2601576d2029d9526e
Abby Henríquez Tejera <paradoja@gmail.com>
2016-08-21 01:41:58 +0200

    6

--- examples/6-clock.elm
+++ examples/6-clock.elm
@@ -1,11 +1,13 @@
 import Html exposing (Html)
 import Html.App as Html
+import Html.Events exposing (onClick)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Time exposing (Time, second)
 
 
 
+main : Program Never
 main =
   Html.program
     { init = init
@@ -19,12 +21,14 @@ main =
 -- MODEL
 
 
-type alias Model = Time
+type alias Model =
+    { time: Time
+    , active: Bool }
 
 
 init : (Model, Cmd Msg)
 init =
-  (0, Cmd.none)
+  (Model 0 True, Cmd.none)
 
 
 
@@ -33,13 +37,20 @@ init =
 
 type Msg
   = Tick Time
+  | Pause
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Tick newTime ->
-      (newTime, Cmd.none)
+      Tick _ ->
+          if model.active then
+              ({ model | time = model.time + 1000 }, Cmd.none)
+          else
+              (model, Cmd.none)
+
+      Pause ->
+          ({ model | active = not model.active }, Cmd.none)
 
 
 
@@ -58,16 +69,35 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
   let
-    angle =
-      turns (Time.inMinutes model)
+    angleMin =
+      turns (Time.inMinutes model.time) - Basics.pi/2
 
-    handX =
-      toString (50 + 40 * cos angle)
+    minHandX =
+      toString (50 + 40 * cos angleMin)
 
-    handY =
-      toString (50 + 40 * sin angle)
+    minHandY =
+      toString (50 + 40 * sin angleMin)
+
+    angleHours =
+      turns (Time.inHours model.time) - Basics.pi/2
+
+    hoursHandX =
+      toString (50 + 30 * cos angleHours)
+
+    hoursHandY =
+      toString (50 + 30 * sin angleHours)
   in
-    svg [ viewBox "0 0 100 100", width "300px" ]
-      [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
-      , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
-      ]
+      Html.div [] [
+           svg [ viewBox "0 0 100 100", width "300px" ]
+               [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
+               , line [ x1 "50", y1 "50", x2 minHandX, y2 minHandY, stroke "#023963" ] []
+               , line [ x1 "50", y1 "50", x2 hoursHandX, y2 hoursHandY, stroke "white" ] []
+               ]
+          , pauseButton model ]
+
+pauseButton : Model -> Html Msg
+pauseButton {active} =
+    let pauseText =
+            if active then "Pause"
+            else "Play"
+    in Html.button [ onClick Pause ] [ text pauseText ]
